b2163e7499867d444a38d7f6a1450eec1d7e7fd4
italo <italo@appbee.online>
2022-09-27 15:49:34 -0300

    $route / useRoute / useRouter

---
+++ src/views/PostsView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="posts">
+    <h1>Posts</h1>
+
+    <ul>
+      <li>
+        <RouterLink to="/postDetail/id1">Post 1</RouterLink>
+      </li>
+      <li>
+        <RouterLink to="/postDetail/id2">Post 2</RouterLink>
+      </li>
+      <li>
+        <RouterLink to="/postDetail/id3">Post 3</RouterLink>
+      </li>
+    </ul>
+  </div>
+</template>
